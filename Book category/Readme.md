aeaf3b07a04798801647be7a8b4249d62d224739
MalinduTharaka <141351242+MalinduTharaka@users.noreply.github.com>
2024-02-05 22:10:18 +0530

    Add files via upload

---
+++ Book category/Readme.md
@@ -0,0 +1,1 @@
+Book category Registration
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome LMS</title>
+    <link rel="stylesheet" href="welcomestyle.css">
+</head>
+<body>
+    <div class="container">
+        <nav>
+            <img src="logowelcome.png" alt="LOGO" class="logo">
+        </nav>
+        <div class="text-box">
+            <p>LMS</p>
+            <h1>WELCOME</h1>
+            <h3>Library Management System</h3>
+            <div class="row">
+                <a href="login\index.php">Logging to system <span>&#x279C;</span></a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
